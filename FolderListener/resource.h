f7c5bb4540624b0c5f2417239d3e29b1cd0d9d81
ALEXANDER POROKHIN <softoad@yandex.ru>
2021-11-07 22:25:06 +0300

    Добавьте файлы проекта.

---
+++ FolderListener/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется FolderListener.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FOLDERLISTENER_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_FOLDERLISTENER              107
+#define IDI_SMALL                       108
+#define IDC_FOLDERLISTENER              109
+#define MY_ID                           110
+#define MY_OK                           111
+#define BROWSE_DIALOG                   112
+#define PATH							113
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDD_D1                          129
+#define IDB_BITMAP1                     131
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
