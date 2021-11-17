b71556b03622c4f3faa8201c9a42258c03c8658f
ALEXANDER POROKHIN <softoad@yandex.ru>
2021-11-17 21:32:39 +0300

    fix icon

--- FolderListener/resource.h
+++ FolderListener/resource.h
@@ -4,7 +4,6 @@
 //
 #define IDC_MYICON                      2
 #define IDD_FOLDERLISTENER_DIALOG       102
-#define ERROR_DIALOG                    141
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -29,6 +28,7 @@
 #define WM_USER_SHELLICON               138
 #define TRAY_MENU_EXIT_ITEM             139
 #define NOTICE_DIALOG                   140
+#define ERROR_DIALOG                    141
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -36,7 +36,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        139
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
